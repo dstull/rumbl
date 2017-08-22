e2fd9ebe24bb9e0584222fbe8a98cb14cfccc6b9
Doug Stull <dstull@Dougs-MBP.home>
2017-08-22 17:41:23 -0400

    end of chapter 7

--- web/models/video.ex
+++ web/models/video.ex
@@ -6,6 +6,8 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User, foreign_key: :user_id
+    
+    belongs_to :category, Rumbl.Category, foreign_key: :category_id
 
     timestamps()
   end
@@ -15,8 +17,8 @@ defmodule Rumbl.Video do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:url, :title, :description])
-    |> validate_required([:url, :title, :description])
+    |> cast(params, [:url, :title, :description, :category_id])
+    |> assoc_constraint(:category)
   end
   
 end
